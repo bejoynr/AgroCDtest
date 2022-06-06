e7bf430af0dc0f745b26fde7cd879f088bdddadd
saurabhsk-pivotchain <saurabh.kharkate@pivotchain.com>
2022-06-06 14:44:01 +0530

    new changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def app2():
-        return 'SaurabhSK'
+        return 'Saurabhsk'
 
 if __name__== "__main__":
         app.run(host='0.0.0.0', port=80, debug=True)
